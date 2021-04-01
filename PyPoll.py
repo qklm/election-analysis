a5cf42561ba67d67dee9b1d9a9a8bde2bf016faf
David <38164159+qklm@users.noreply.github.com>
2021-04-01 08:26:50 -0700

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -6,10 +6,27 @@ file_to_load = os.path.join("resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        vote_percentage = str(round(vote_percentage,1)
+        print(f"{candidate_name}: recieved {vote_percentage}% of the vote")
+print(candidate_votes)
